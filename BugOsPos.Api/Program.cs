beb8567506fab5c451de8c3033c05f29bbaae6d9
ArmintasP <75906786+ArmintasP@users.noreply.github.com>
2022-12-27 02:51:05 +0200

    Add swagger comments.

--- BugOsPos.Api/Program.cs
+++ BugOsPos.Api/Program.cs
@@ -3,6 +3,9 @@ using BugOsPos.Api.Mappings;
 using BugOsPos.Application;
 using BugOsPos.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -15,7 +18,19 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddControllers();
 
     builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
+    builder.Services.AddSwaggerGen(options =>
+    {
+        var xmlDocumentationFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlDocumentationFile));
+
+        options.DescribeAllParametersInCamelCase();
+
+        options.MapType<TimeSpan>(() => new OpenApiSchema
+        {
+            Type = "string",
+            Example = new OpenApiString("00:00:00")
+        });
+    });
 }
 
 var app = builder.Build();
