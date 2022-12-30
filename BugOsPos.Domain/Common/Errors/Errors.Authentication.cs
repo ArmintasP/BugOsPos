626eb5e083f1eece3039866d4a2e7a7d076b2b91
ArmintasP <75906786+ArmintasP@users.noreply.github.com>
2022-12-30 12:06:48 +0200

    Fix mismatching class names and file names.

--- BugOsPos.Domain/Common/Errors/Errors.Authentication.cs
+++ BugOsPos.Domain/Common/Errors/Errors.Authentication.cs
@@ -3,14 +3,10 @@
 namespace BugOsPos.Domain.Common.Errors;
 public static partial class Errors
 {
-    public static class Customer
+    public static class Authentication
     {
-        public static Error DuplicateEmail => Error.Conflict(
-            code: "Customer.DuplicateEmail",
-            description: "The email is already in use by another customer.");
-
-        public static Error DuplicateUsername => Error.Conflict(
-            code: "Customer.DuplicateUsername",
-            description: "The username is already in use by another customer.");
+        public static Error InvalidCredentials => Error.Conflict(
+            code: "Authentication.InvalidCredentials",
+            description: "Invalid username or password.");
     }
 }
