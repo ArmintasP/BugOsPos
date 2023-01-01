0232a75588d5b652be9c76bd35f527d0f2a08da8
ArmintasP <75906786+ArmintasP@users.noreply.github.com>
2023-01-01 21:36:06 +0200

    Initial commit.

---
+++ BugOsPos.Api/Controllers/GroupsController.cs
@@ -0,0 +1,40 @@
+﻿using BugOsPos.Application.Groups;
+using BugOsPos.Infrastructure.Authentication;
+using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugOsPos.Api.Controllers;
+
+[ApiController]
+public sealed  class GroupsController : ApiController
+{
+    private readonly IMapper _mapper;
+    private readonly ISender _mediator;
+
+    public GroupsController(IMapper mapper, ISender mediator)
+    {
+        _mapper = mapper;
+        _mediator = mediator;
+    }
+
+    [HttpPost("groups/{id}")]
+
+    public async Task<IActionResult> GetGroupById(int id)
+    {
+        if (GetClaimValue(JwtSettings.EmployeeClaim) is null)
+            return Problem(new() { Domain.Common.ErrorsCollection.Errors.Employee.Forbidden });
+
+        if (GetClaimValue(JwtSettings.FranchiseClaim) is not string franchiseIdString)
+            return Problem(new() { Domain.Common.ErrorsCollection.Errors.Authentication.FranchiseIdMissing });
+        if (!int.TryParse(franchiseIdString, out var franchiseId))
+            return Problem(new() { Domain.Common.ErrorsCollection.Errors.Authentication.InvalidFranchiseId });
+        
+        var query =  new GetGroupByIdQuery(id, franchiseId);
+        var result = await _mediator.Send(query);
+
+        return result.Match(
+            result => Ok(_mapper.Map<GetGroupByIdResponse>(result)),
+            errors => Problem(errors));
+    }
+}
---
+++ BugOsPos.Application/Groups/GetGroupByIdQuery.cs
@@ -0,0 +1,47 @@
+﻿using BugOsPos.Application.Common.Interfaces.Persistence;
+using BugOsPos.Application.Employees;
+using BugOsPos.Domain.FranchiseAggregate.ValueObjects;
+using BugOsPos.Domain.GroupAggregate;
+using BugOsPos.Domain.GroupAggregate.ValueObjects;
+using ErrorOr;
+using FluentValidation;
+using MediatR;
+
+namespace BugOsPos.Application.Groups;
+
+public sealed record GetGroupByIdQuery(int Id, int FranchiseId) : IRequest<ErrorOr<GetGroupResult>>;
+
+public sealed record GetGroupResult(Group Group);
+
+public sealed class GetGroupByIdValidator : AbstractValidator<GetGroupByIdQuery>
+{
+    public GetGroupByIdValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty();
+        RuleFor(x => x.FranchiseId).NotEmpty();
+    }
+}
+
+public sealed class GetGroupByIdQueryHandler : IRequestHandler<GetGroupByIdQuery, ErrorOr<GetGroupResult>>
+{
+    private readonly IGroupRepository _groupRepository;
+
+    public GetGroupByIdQueryHandler(IGroupRepository groupRepository)
+    {
+        _groupRepository = groupRepository;
+    }
+
+    public async Task<ErrorOr<GetGroupResult>> Handle(GetGroupByIdQuery request, CancellationToken cancellationToken)
+    {
+        var groupId = GroupId.New(request.Id);
+        var franchiseId = FranchiseId.New(request.FranchiseId);
+        
+        if (await _groupRepository.GetGroupById(groupId) is not Group group ||
+            group.FranchiseId != franchiseId)
+        {
+            return Domain.Common.ErrorsCollection.Errors.Group.BadFranchiseId;
+        }
+        
+        return new GetGroupResult(group);
+    }
+}
