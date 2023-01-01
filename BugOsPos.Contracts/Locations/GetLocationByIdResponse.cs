65391e11fd4aef1e3b7c79d14b5c9fab8c6dfedd
pijus petkevicius <pijus.petkevicius@bentley.com>
2023-01-01 21:27:06 +0200

    location 1 endpoint

---
+++ BugOsPos.Contracts/Locations/GetLocationByIdResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace BugOsPos.Contracts.Locations;
+public sealed record GetLocationByIdResponse(
+    int Id,
+    string Name,
+    string Address,
+    Rating Rating,
+    List<string> PhotoPaths,
+    List<NormalWorkingHours> NormalWorkingHours,
+    List<OverriddenWorkingHours> OverriddenWorkingHours);
+
+
+public sealed record Rating(
+    decimal Value,
+    int NumberOrRatings);
+
+
+public sealed record NormalWorkingHours(
+    TimeOnly OpeningTime,
+    TimeOnly ClosingTime);
+
+public sealed record OverriddenWorkingHours(
+    TimeOnly AltOpeningTime,
+    TimeOnly AltClosingTime,
+    DateTime ApplyAltTimesStartDate,
+    DateTime ApplyAltTimesEndDate,
+    bool Closed);
