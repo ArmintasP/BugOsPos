af8fe502119eca720ede3908965d964fdb245734
ago-ble <agota.luzyte@gmail.com>
2023-01-03 01:35:45 +0200

    .

---
+++ BugOsPos.Api/Mappings/ProductMapping.cs
@@ -0,0 +1,18 @@
+﻿using BugOsPos.Application.Products;
+using BugOsPos.Contracts.Products;
+using Mapster;
+
+namespace BugOsPos.Api.Mappings;
+
+public sealed class ProductMapping : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<GetProductByIdResult, GetProductByIdResponse>()
+            .Map(dest => dest, src => src.Product)
+            .Map(dest => dest.Id, src => src.Product.Id.Get())
+            .Map(dest => dest.FranchiseId, src => src.Product.FranchiseId.Get())
+            .Map(dest => dest.CategoryId, src => src.Product.CategoryId.Get())
+            .Map(dest => dest.DiscountId, src => src.Product.DiscountId.Get());
+    }
+}
